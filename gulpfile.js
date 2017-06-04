a836321e980160f989c99fae22c593095a79d7e9
vicente <vicente.noriega.sc@gmail.com>
2017-06-04 02:46:36 -0500

    28. Creando un servidor con BrowserSync y Livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require( 'gulp' ); // dependencia
-var sass = require( 'gulp-sass' ); // dependencia
+var gulp = require( 'gulp' ); // agregamos la dependencia
+var sass = require( 'gulp-sass' ); // agregamos la dependencia
+var browserSync = require( 'browser-sync' ); // agregamos la dependencia
+var reload = browserSync.reload; // propiedad de browserSync
 
 /* definimos la tarea sass */
 gulp.task( 'sass', function() {
@@ -8,11 +10,22 @@ gulp.task( 'sass', function() {
 	gulp.src( 'sass/app.sass' )
 		.pipe( sass({
 
-			includePaths: ['sass']
+			includePaths: [ 'sass' ]
 		}) )
 		// destino del archivo
 		.pipe( gulp.dest( 'app/css' ) );
-});
+} );
+
+/* difinimos una tarea nueva, la cual se va a encargar de realizar los cambios en el navegador en tiempo real. Indicando el nombre de la tarea y como segundo paramatro la tarea que ejecuta antes de ejecutar la función */
+gulp.task( 'serve', [ 'sass' ], function() {
+
+	browserSync.init([ "app/css/*.css", "app/js/*.js", "*.html" ], {
+		
+		server: {
+			baseDir: 'app'
+		}
+	}); // fin del browserSyn.init
+} );
 
 /* agrega la tarea para cuando en la terminar cuando corramos el comando "gulp" se realice esta tarea */
-gulp.task( 'default', [ 'sass' ] );
+gulp.task( 'default', [ 'serve' ] );
