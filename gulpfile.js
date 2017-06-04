918af71dc77827084283940e805b491f1c8bcd49
vicente <vicente.noriega.sc@gmail.com>
2017-06-04 04:31:55 -0500

    30. Agregando autoprefixer a nuestro proyecto workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require( 'gulp' ); // agregamos la dependencia
 var sass = require( 'gulp-sass' ); // agregamos la dependencia
 var browserSync = require( 'browser-sync' ); // agregamos la dependencia
 var reload = browserSync.reload; // propiedad de browserSync
+var autoprefixer = require( 'gulp-autoprefixer' ); // agregamos la dependencia
 
 /* definimos la tarea sass */
 gulp.task( 'sass', function() {
 
 	// origen del archivo
-	gulp.src( 'sass/app.sass' )
+	gulp.src( 'scss/app.scss' )
+		.pipe(autoprefixer()
+    	)
 		.pipe( sass({
 
-			includePaths: [ 'sass' ]
+			includePaths: [ 'scss' ]
 		}) )
 		// destino del archivo
 		.pipe( gulp.dest( 'app/css' ) );
@@ -19,7 +22,7 @@ gulp.task( 'sass', function() {
 /* difinimos una tarea nueva, la cual se va a encargar de realizar los cambios en el navegador en tiempo real. Indicando el nombre de la tarea y como segundo paramatro la tarea que ejecuta antes de ejecutar la función */
 gulp.task( 'serve', [ 'sass' ], function() {
 
-	browserSync.init([ "app/css/*.css", "app/js/*.js", "*.html" ], {
+	browserSync.init([ "app/css/*.css", "app/js/*.js", "app/*.html" ], {
 		
 		server: {
 			baseDir: 'app'
@@ -27,5 +30,12 @@ gulp.task( 'serve', [ 'sass' ], function() {
 	}); // fin del browserSyn.init
 } );
 
+/* creamos la tarea que se va a encargar de estar revisando los cambios en nuestros archivos */
+gulp.task( 'watch', [ 'sass','serve' ], function(){
+
+	/* para poder escuchar los cambios en sass, necesita ejecutar la tarea [ 'sass' ] */
+	gulp.watch( [ 'scss/*.scss' ], [ 'sass' ] );
+} );
+
 /* agrega la tarea para cuando en la terminar cuando corramos el comando "gulp" se realice esta tarea */
-gulp.task( 'default', [ 'serve' ] );
+gulp.task( 'default', [ 'watch' ] );
